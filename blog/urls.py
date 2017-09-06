d00c4e13ae69f6bab37913134f7414f1ed70b6ee
saturn <saturn721@gmail.com>
2017-09-06 21:44:59 +0500

    third download site

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),#ищет представление в views.py
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
 ]
