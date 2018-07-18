50c3f914cd1577df7b8fccd405a9235ef88b1018
Unknown <saturn721@gmail.com>
2018-07-18 14:56:03 +0500

    web-extension start

---
+++ countursy/content_scripts/stage.js
@@ -0,0 +1,13 @@
+var video = document.getElementById('video');
+console.log(video);
+if (video.paused == true) {
+    // Play the video
+    video.play();
+
+
+  } else {
+    // Pause the video
+    video.pause();
+
+
+};
---
+++ countursy/content_scripts/zalipon.js
@@ -0,0 +1,39 @@
+
+
+ function getVideo() {
+
+
+
+   var video = document.getElementById('video');
+   var canvas = document.getElementById('canvas');
+   var context = canvas.getContext('2d');
+   var tracker = new tracking.ObjectTracker('face');
+    tracker.setInitialScale(4);
+    tracker.setStepSize(2);
+    tracker.setEdgesDensity(0.1);
+    tracking.track('#video', tracker, { camera: false });
+    tracker.on('track', function(event) {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      event.data.forEach(function(rect) {
+        context.strokeStyle = '#a64ceb';
+        context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+        context.font = '11px Helvetica';
+        context.fillStyle = "#fff";
+        context.fillText('x: ' + rect.x + 'пик', rect.x + rect.width + 5, rect.y + 11);
+
+        context.fillText('y: ' + rect.y + 'пик', rect.x + rect.width + 5, rect.y + 22);
+      });
+    });
+
+    guido(tracker);
+ video.play();
+
+
+ };
+ function guido(tracker){
+     var gui = new dat.GUI();
+     gui.add(tracker, 'edgesDensity', 0.1, 0.5).step(0.01);
+     gui.add(tracker, 'initialScale', 1.0, 10.0).step(0.1);
+     gui.add(tracker, 'stepSize', 1, 5).step(0.1);
+ };
+window.onload = getVideo();
